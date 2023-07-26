dd6393f0a63d3d21917b355227572f118ffceec7
artkon <artemkonopelko@gmail.com>
2023-07-27 02:53:11 +0400

    feat: add env variable to app, use for port

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
+import dotenv from 'dotenv';
+import process from 'process';
 import { AppModule } from './app.module';
+
+
+dotenv.config();
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-    await app.listen(4000);
+    await app.listen(process.env.port || 4000);
 }
 bootstrap();
