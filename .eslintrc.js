658b5c0acb1c257f97cef5cd8c8138874daef127
artkon <artemkonopelko@gmail.com>
2023-07-27 02:11:21 +0400

    feat: scaffold User, Track, Artist, Album, Favs entities

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,6 +20,7 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
-    'prettier/prettier': ['error', { 'singleQuote': true, 'tabWidth': 4 }],
+    indent: ['error', 4],
+    'prettier/prettier': ['error', { singleQuote: true, tabWidth: 4 }],
   },
 };
