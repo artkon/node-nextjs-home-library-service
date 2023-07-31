46e6685d224f85cb814fec386e058f08f667a898
artkon <artemkonopelko@gmail.com>
2023-07-31 23:35:20 +0400

    refactor: refactor api.yaml file

--- README.md
+++ README.md
@@ -25,8 +25,7 @@ Create `.env` file from `.env.example`
 npm start
 ```
 
-After starting the app on port (4000 as default) you can open
-in your browser OpenAPI documentation by typing http://localhost:4000/api/.
+After starting the app on port (4000 as default).
 For more information about OpenAPI/Swagger please visit https://swagger.io/.
 
 ## Testing
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
 import { NestFactory } from '@nestjs/core';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import dotenv from 'dotenv';
 import process from 'process';
 import { AppModule } from './app.module';
@@ -9,14 +8,6 @@ dotenv.config();
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-
-    const config = new DocumentBuilder()
-        .setTitle('Home Library Service')
-        .setDescription('Home music library service')
-        .setVersion('1.0.0')
-        .build();
-    const document = SwaggerModule.createDocument(app, config);
-    SwaggerModule.setup('api', app, document);
 
     const port = process.env.port || 4000;
 
