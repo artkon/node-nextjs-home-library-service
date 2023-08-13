74fc419bed06f711d65dfdb02a2d1d7a2e1248d5
artkon <artemkonopelko@gmail.com>
2023-08-14 00:44:57 +0400

    feat: fix indents style, and .env in .gitignore, refactor nest-cli config

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,7 +19,7 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
-    indent: ['error', 4],
+    "indent": ["error", 4, { "SwitchCase": 1, "ignoredNodes": ["PropertyDefinition"] }],
     "no-multiple-empty-lines": ["error", { "max": 2, "maxEOF": 1 }],
     "@typescript-eslint/no-unused-vars": ["error", { "argsIgnorePattern": "^_" }],
   },
